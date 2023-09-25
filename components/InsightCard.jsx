ff0b08a20036e22eb6440f60b5426d8fced791b1
Vathila Rajapakse <87406802+VathilaRajapakse@users.noreply.github.com>
2023-09-25 15:20:34 +0530

    commit

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -7,8 +7,7 @@ import { fadeIn } from '../utils/motion';
 const InsightCard = ({ imgUrl, title, subtitle, index }) => (
   <motion.div
     variants={fadeIn('up', 'spring', index * 0.5, 1)}
-    className=
-    "flex md:flex-row flex-col gap-4"
+    className= "flex md:flex-row flex-col gap-4"
   >
     <img
       src={imgUrl}
@@ -21,6 +20,7 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
           {title}
         </h4>
         <p className="mt-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white">
+          
           {subtitle}
         </p>
       </div>
