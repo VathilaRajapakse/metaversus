bef9ea7d260ae29b26bec77339955728959774c5
Vathila Rajapakse <87406802+VathilaRajapakse@users.noreply.github.com>
2023-09-25 12:54:00 +0530

    commit

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -18,7 +18,8 @@ const Explore = () => {
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
-        className={`${styles.innerWidth} mx-auto flex flex-col`}
+        className={`${styles.innerWidth} 
+        mx-auto flex flex-col`}
       >
         <TypingText title="| The World" textStyles="text-center" />
         <TitleText
